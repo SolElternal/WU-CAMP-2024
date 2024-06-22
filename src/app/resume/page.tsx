371b82b63af2da89fd3d43117057fc751c251526
Teeranai Sangtaera <teeranaisangtaera@gmail.com>
2024-06-22 22:00:59 +0700

    making resume page and css

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,136 @@
+import "./style_resume.css";
+
+export default function Resume(){
+    return(
+        <main className='flex min-h-screen flex-col p-24 font-serif bg-gradient-to-r from-cyan-500 to-blue-500'>
+            <div className='flex justify-center'>
+                <div className="m-4 h-[950px] w-[650px]  bg-gray-50">
+                    <p className='text-center margin-top-name text-2xl text-blue-900'>Teeranai Sangtaera</p>
+                    <p className='text-center text-lg text-blue-900'>Programmer</p>
+
+                    <table>
+                    <tr>
+                        <td className='vertical-line column-line'>
+                            <img src='getstudentimage.jpg' height='40%' width='55%' className='margin-image-profile'></img>
+
+                            <div className='margin-infornation text-xl text-blue-700'>
+                                Contact
+                            </div>
+                            <div className='margin-infornation-sub flex text-sm text-slate-600'>
+                                <img src="https://png.pngtree.com/png-clipart/20230418/original/pngtree-student-line-icon-png-image_9065647.png" width="9%"></img>
+                                <p className='margin-sub-font'>&nbsp;&nbsp;Student</p>
+                            </div>
+                            <div className='margin-infornation-sub flex text-sm text-slate-600'>
+                            <img src="https://i.pinimg.com/736x/4a/24/14/4a2414e8d912d269ddb8a55b3de2c00b.jpg" width="9%"></img>
+                                <p className='margin-sub-font'>&nbsp;&nbsp;Thailand,TH</p>
+                            </div>
+                            <div className='margin-infornation-sub flex text-sm text-slate-600'>
+                            <img src="https://banner2.cleanpng.com/20180526/eio/kisspng-email-computer-icons-gmail-5b093a2abbd322.5464747815273313707693.jpg" width="9%"></img>
+                                <p className='margin-sub-font'>&nbsp;&nbsp;teeranaisangtaera@gmail.com</p>
+                            </div>
+                            <div className='margin-infornation-sub flex text-sm text-slate-600'>
+                                &nbsp;<img src="https://www.freeiconspng.com/thumbs/phone-icon/office-phone-icon--25.png" width="5%" height='1%'></img>
+                                <p className='margin-sub-font'>&nbsp;&nbsp;0884424086</p>
+                            </div>
+
+
+                            <div className='margin-infornation text-xl text-blue-700'>
+                                Languages
+                            </div>
+                            <div className='margin-infornation-sub flex text-sm'>
+                                <p className='margin-sub-font text-amber-600'>Thailand&nbsp;:&nbsp;Native</p>
+                            </div>
+                            <div className='margin-infornation-sub flex text-sm'>
+                                <p className='margin-sub-font text-pink-800'>English&nbsp;&nbsp;&nbsp;:&nbsp;Intermediate</p>
+                            </div>
+                            
+
+                            <div className='margin-infornation text-xl text-blue-700'>
+                                Skills
+                            </div>
+                            <div className='margin-infornation-sub flex text-sm text-slate-600'>
+                                <p className='margin-sub-font'>Adobe Photoshop</p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<div className=" h-[28px] w-[28px]  bg-green-600 rounded-lg text-small-na">90&nbsp;%</div>
+                            </div>
+                            <div className='margin-infornation-sub flex text-sm text-slate-600'>
+                                <p className='margin-sub-font'>Html Language</p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<div className=" h-[28px] w-[28px]  bg-green-500 rounded-lg text-small-na">75&nbsp;%</div>
+                            </div>
+                            <div className='margin-infornation-sub flex text-sm text-slate-600'>
+                                <p className='margin-sub-font'>C++ Language</p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<div className=" h-[28px] w-[28px]  bg-green-500 rounded-lg text-small-na">75&nbsp;%</div>
+                            </div>
+                            <div className='margin-infornation-sub flex text-sm text-slate-600'>
+                                <p className='margin-sub-font'>Java Language</p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<div className=" h-[28px] w-[28px]  bg-green-400 rounded-lg text-small-na">70&nbsp;%</div>
+                            </div>
+                            <div className='margin-infornation-sub flex text-sm text-slate-600'>
+                                <p className='margin-sub-font'>Python Language</p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<div className=" h-[28px] w-[28px]  bg-green-300 rounded-lg ">60&nbsp;%</div>
+                            </div>
+                            <div className='margin-infornation-sub flex text-sm text-slate-600'>
+                                <p className='margin-sub-font'>MySQL</p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<div className=" h-[28px] w-[28px]  bg-green-300 rounded-lg ">60&nbsp;%</div>
+                            </div>
+
+                            
+                        </td>
+                        <td className='column-line2'>
+                            <div className='margin-infornation3 text-xl flex text-blue-700'>
+                                Training experience<img src="https://png.pngtree.com/png-vector/20191021/ourmid/pngtree-vector-bag-icon-png-image_1834307.jpg" width="10%"></img>
+                            </div>
+                            <div className='margin-infornation3 text-mg flex margin-sub-font text-blue-500'>
+                                Training in programming in C language
+                            </div>
+                            <div className='margin-infornation3 text-mg flex margin-sub-font text-amber-600'>
+                            <img src="https://t3.ftcdn.net/jpg/05/54/60/68/360_F_554606861_txKoTSp8aBaO8oARngeSbgNcSB1yWTRc.jpg" width="6%"></img>&nbsp;
+                                || 23 July 2565
+                            </div>
+                            <div className='margin-infornation3 text-sm flex margin-sub-font text-slate-600'>
+                                &nbsp;&nbsp;This camp helps develop all the basics of C programming. and along with telling various techniques for writing programs efficiently and quickly In this camp, I gained a lot of knowledge.
+                            </div>
+
+                            <div className='margin-infornation3 text-mg flex margin-sub-font2 text-blue-500'>
+                                Scrum work camp
+                            </div>
+                            <div className='margin-infornation3 text-mg flex margin-sub-font text-amber-600'>
+                            <img src="https://t3.ftcdn.net/jpg/05/54/60/68/360_F_554606861_txKoTSp8aBaO8oARngeSbgNcSB1yWTRc.jpg" width="6%"></img>&nbsp;
+                                || 18 Auguest 2566
+                            </div>
+                            <div className='margin-infornation3 text-sm flex margin-sub-font text-slate-600'>
+                                &nbsp;&nbsp;This camp from participating in activities Gain experience in working as a team. With the scrum method, there is organized and efficient way of working. It also helps bring out the efficiency of everyone in each aspect to help make the team's <br></br>work come out perfectly and according to process.
+                            </div>
+
+                            <div className='margin-infornation3 text-mg flex margin-sub-font2 text-blue-500'>
+                                Js programming camp
+                            </div>
+                            <div className='margin-infornation3 text-mg flex margin-sub-font text-amber-600'>
+                            <img src="https://t3.ftcdn.net/jpg/05/54/60/68/360_F_554606861_txKoTSp8aBaO8oARngeSbgNcSB1yWTRc.jpg" width="6%"></img>&nbsp;
+                                || 21 June 2567
+                            </div>
+                            <div className='margin-infornation3 text-sm flex margin-sub-font text-slate-600'>
+                                &nbsp;&nbsp;This camp is very beneficial to us. Because this <br></br>camp has provided knowledge about making <br></br>websites, both font-end and back-end, along with various recommended techniques or accessories. <br></br>All are good for self-development.
+                            </div>
+
+                            <div className='margin-infornation3 text-xl flex margin-sub-font2 text-blue-700'>
+                                Performance&nbsp;<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOsOmcG_8xjVgpJTsQAv9xMtkEdWAvag-RgUIPP5YL0g&s" width="7%"></img>
+                            </div>
+                            <div className="margin-infornation3 text-sm  margin-sub-font2 text-slate-600">
+                                    <div className="flex">
+                                    <ul>
+                                        <li>Join your university's IEEE club. and holds the position of secretary</li><br></br>
+                                    </ul>
+                                    </div>
+                                    <div>
+                                    <ul>
+                                        <li>There is a project to create a tourism website for Trang Province.</li><br></br>
+                                    </ul>
+                                    <div>
+                                    <ul>
+                                        <li>Created a Smart bin project to send IOT data on the web in real time.</li><br></br>
+                                    </ul>
+                                    </div>
+                                    </div>
+                          </div>
+                        </td>
+                    </tr>
+                </table>
+                </div>
+            </div>
+        </main>
+    );
+}
