b3d321b0b2da0ad9fa1f6a18d2ece38d27c10ed6
Teeranai Sangtaera <teeranaisangtaera@gmail.com>
2024-06-22 19:20:46 +0700

    create blogs 3 paragraph

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,142 @@
+import "./style.css";
+
+export default function Resume(){
+    return (
+        <main className="flex min-h-screen flex-col p-24 font-serif">
+            
+            <title>Blog</title>
+            <div className='flex justify-center'>
+                    <div className="m-6 h-[180px] w-[750px]  bg-zinc-300 rounded-lg ">
+                        <p className='text-sm'>&nbsp;&nbsp;&nbsp;Sofia Fanah</p>
+                        
+                        <div className="flex flex-nowrap ">
+                                <img src="profile_blog.webp" height="100px" width="100px"></img>
+                                <div className="message-margin">&nbsp;&nbsp;&nbsp;&nbsp;Hello Day, I've been learning programming for two days. with older siblings who had studied at the same university It's a very good experience.</div>
+                        </div>
+                        <p className="text-sm">
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Jun 21
+                        </p>
+
+                        <div className="flex m-6 h-[25px] w-[100px]  bg-zinc-100 rounded-lg  capitalize flex-nowrap">
+                            <div className="text-red-800">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Like</div>
+                            &nbsp;&nbsp;&nbsp;
+                            <img src='heart-6114039_960_720.webp' height='40px' width='40px'></img>
+                            <div>
+                                <span className="relative flex h-3 w-3">
+                                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                                    <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+                                </span>
+                                
+                            </div>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <img src='line.png' height='70' width='70'></img>&nbsp;&nbsp;
+                                <img src='62-instagram-512.webp' height='70' width='70'></img>&nbsp;&nbsp;
+                                <img src='facebook_icon.webp' height='70' width='70'></img>
+
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <img src='mes_icon.webp' height='90' width='90'></img>
+                                <div className='text-sm message-margin3'>
+                                    &nbsp;102 
+                                </div>
+                                <div className='text-sm message-margin3'>
+                                    &nbsp;Comment
+                                </div>
+
+                                <div className='message-margin2 text-sm message-margin3'>4&nbsp;mins&nbsp;read</div>
+                                
+                                <img src='bookmark.webp' height='90' width='90' className="image-margin1"></img>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='flex justify-center'>
+                    <div className="m-6 h-[180px] w-[750px]  bg-zinc-300 rounded-lg ">
+                        <p className='text-sm'>&nbsp;&nbsp;&nbsp;Hanna Jisak</p>
+                        
+                        <div className="flex flex-nowrap image-margin2">
+                                <img src="unnamed.png" height="75px" width="80px"></img>
+                                <div className="message-margin">&nbsp;&nbsp;&nbsp;&nbsp;Hello Day, I've been learning programming for two days. with older siblings who had studied at the same university It's a very good experience.</div>
+                        </div>
+                        <p className="text-sm">
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Jun 21
+                        </p>
+
+                        <div className="flex m-6 h-[25px] w-[100px]  bg-zinc-100 rounded-lg  capitalize flex-nowrap">
+                            <div className="text-red-800">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Like</div>
+                            &nbsp;&nbsp;&nbsp;
+                            <img src='heart-6114039_960_720.webp' height='40px' width='40px'></img>
+                            <div>
+                                <span className="relative flex h-3 w-3">
+                                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                                    <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+                                </span>
+                                
+                            </div>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <img src='line.png' height='70' width='70'></img>&nbsp;&nbsp;
+                                <img src='62-instagram-512.webp' height='70' width='70'></img>&nbsp;&nbsp;
+                                <img src='facebook_icon.webp' height='70' width='70'></img>
+
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <img src='mes_icon.webp' height='90' width='90'></img>
+                                <div className='text-sm message-margin3'>
+                                    &nbsp;89
+                                </div>
+                                <div className='text-sm message-margin3'>
+                                    &nbsp;Comment
+                                </div>
+
+                                <div className='message-margin2 text-sm message-margin3'>12&nbsp;mins&nbsp;read</div>
+                                
+                                <img src='bookmark.webp' height='90' width='90' className="image-margin1"></img>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='flex justify-center'>
+                    <div className="m-6 h-[180px] w-[750px]  bg-zinc-300 rounded-lg ">
+                        <p className='text-sm'>&nbsp;&nbsp;&nbsp;Satoru Naha</p>
+                        
+                        <div className="flex flex-nowrap image-margin2">
+                                <img src="gojo.png" height="75px" width="80px"></img>
+                                <div className="message-margin">&nbsp;&nbsp;&nbsp;&nbsp;Hello Day, I've been learning programming for two days. with older siblings who had studied at the same university It's a very good experience.</div>
+                        </div>
+                        <p className="text-sm">
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Jun 21
+                        </p>
+
+                        <div className="flex m-6 h-[25px] w-[100px]  bg-zinc-100 rounded-lg  capitalize flex-nowrap">
+                            <div className="text-red-800">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Like</div>
+                            &nbsp;&nbsp;&nbsp;
+                            <img src='heart-6114039_960_720.webp' height='40px' width='40px'></img>
+                            <div>
+                                <span className="relative flex h-3 w-3">
+                                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                                    <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+                                </span>
+                                
+                            </div>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <img src='line.png' height='70' width='70'></img>&nbsp;&nbsp;
+                                <img src='62-instagram-512.webp' height='70' width='70'></img>&nbsp;&nbsp;
+                                <img src='facebook_icon.webp' height='70' width='70'></img>
+
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <img src='mes_icon.webp' height='90' width='90'></img>
+                                <div className='text-sm message-margin3'>
+                                    &nbsp;298
+                                </div>
+                                <div className='text-sm message-margin3'>
+                                    &nbsp;Comment
+                                </div>
+
+                                <div className='message-margin2 text-sm message-margin3'>7&nbsp;mins&nbsp;read</div>
+                                
+                                <img src='bookmark.webp' height='90' width='90' className="image-margin1"></img>
+                        </div>
+                    </div>
+                </div>
+            
+        </main>
+    );
+}
